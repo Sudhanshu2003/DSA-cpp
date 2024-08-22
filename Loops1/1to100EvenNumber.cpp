f8dae3081104d58bd8d6c1363c066137007948b4
Sudhanshu Kumar <147744930+Sudhanshu2003@users.noreply.github.com>
2024-08-22 17:47:49 +0530

    Add files via upload

---
+++ Loops1/1to100EvenNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 1; i<=100; i+=1){
+        if(i%2==0)
+        cout<<i<<endl; 
+    } 
+
+
+    //1 to 100 print odd number
+    for(int i = 1; i<=100; i+=1){
+        if(i%2!=0)
+        cout<<i<<endl; 
+    } 
+}
