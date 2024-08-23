1c41749b1e33c7a2906d88cb3a91bf6c269ad659
Sudhanshu Kumar <147744930+Sudhanshu2003@users.noreply.github.com>
2024-08-23 18:54:13 +0530

    Add files via upload

---
+++ Loops2/factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n; 
+    // cout<<"Enter the Number : ";
+    // cin>>n;
+    //  int product = 1;
+    // for(int i=1; i<=n; i++){
+    //     product *= i;
+    // }
+    // cout<<product;
+
+
+    int product = 1;
+    for(int i=1; i<=100; i++){
+        product *= i;
+       //cout<<i<<endl;
+    }
+    cout<<product;
+}
