19f63a5c77cf1f4a653ae908482a3c56bac54359
Sudhanshu Kumar <147744930+Sudhanshu2003@users.noreply.github.com>
2024-09-04 16:13:56 +0530

    Add files via upload

---
+++ Array/sortNozAndNoo.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void sort01(vector<int>&v){
+    int noo=0;
+    int noz=0;
+    for(int i=0; i<v.size(); i++){
+    if(v[i]==0) noz++;
+    else noo++;
+    }
+    for(int i=0; i<v.size(); i++){
+        if(i<noz) v[i]=0;
+        else v[i]=1;
+    }
+}
+void sort01m2(vector<int>&v){
+    int n=v.size();
+    int i=0;
+    int j=n-1;
+    while(i<j){
+        if(v[i]==0) i++;
+        else if(v[j]==1) j--;
+        else if(v[i]==1 && v[j]==0){
+            v[i]=0;
+            v[j]=1;
+        }
+    }
+}
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(1);
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+   // sort01(v);
+    sort01m2(v);
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
