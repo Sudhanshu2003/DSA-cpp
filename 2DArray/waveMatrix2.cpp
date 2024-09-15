afb6570ee4432bbcfdf0873f5ce613cff9c6079d
Sudhanshu Kumar <147744930+Sudhanshu2003@users.noreply.github.com>
2024-09-15 19:13:58 +0530

    Add files via upload

---
+++ 2DArray/waveMatrix2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no of rows : ";
+    cin>>n;
+    int m;
+    cout<<"Enter the no of columns : ";
+    cin>>m;
+    int arr[n][m];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+       //  Wave Form
+    for(int i=0; i<n; i++){
+        if(i%2==0){
+            for(int j=n-1; j>=0; j--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        else{
+            for(int j=0; j<m; j++){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
