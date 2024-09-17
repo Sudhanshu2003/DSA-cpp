520e1c4d54002e540ecb16463dbcd183defad9c2
Sudhanshu Kumar <147744930+Sudhanshu2003@users.noreply.github.com>
2024-09-17 19:04:30 +0530

    Add files via upload

---
+++ String/stringAsAdatatype.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string str = "Shivam Thakur";
+    // cout<<str;
+    // cout<<str[0];
+
+    // input without loop
+    string s;
+    // cin>>s;   //  only if the given string is no space
+    getline(cin,s);
+    cout<<s;
+}
